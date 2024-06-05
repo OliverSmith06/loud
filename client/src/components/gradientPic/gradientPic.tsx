5abdad3848e1d23ad38b1129b67668b8e3158b5d
Oliver Smith <z5364166@ad.unsw.edu.au>
2024-06-06 02:53:23 +1000

    fix background

--- client/src/components/gradientPic/gradientPic.tsx
+++ client/src/components/gradientPic/gradientPic.tsx
@@ -1,12 +1,12 @@
 import ProgressBar from "../progressBar/progressBar";
 import Timer from "../timer/timer";
 import styles from "./gradientPic.module.css";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 function GradientPic() {
-  const startDate = new Date('2024-02-27T18:00:00'); // Replace with your start date
-  const endDate = new Date('2024-06-07T08:40:00');   // Replace with your end date
-  
+  const startDate = new Date("2024-02-27T18:00:00"); // Replace with your start date
+  const endDate = new Date("2024-06-14T08:20:00"); // Replace with your end date 06/14/2024 08:20:00
+
   // Calculate the total time difference in milliseconds
   const totalTime: number = +endDate - +startDate;
 
@@ -14,14 +14,16 @@ function GradientPic() {
   const [percentage, setPercentage] = useState(0);
 
   const roundHQuarter = (num: number) => {
-    return Math.round(num*4)/4;
-  }
+    return Math.round(num * 4) / 4;
+  };
 
   const calculatePercentage = () => {
     const currentTime = new Date();
     const elapsedTime: number = +currentTime - +startDate;
-    const currentPercentage: number = roundHQuarter((elapsedTime / totalTime) * 100);
-    console.log(percentage)
+    const currentPercentage: number = roundHQuarter(
+      (elapsedTime / totalTime) * 100
+    );
+    console.log(percentage);
     if (currentPercentage < 100) {
       setPercentage(currentPercentage);
     }
@@ -30,7 +32,7 @@ function GradientPic() {
   // Use useEffect to run the calculation function and update every minute
   useEffect(() => {
     calculatePercentage(); // Initial calculation
-    
+
     const intervalId = setInterval(calculatePercentage, 100); // Update every minute
 
     return () => {
@@ -38,15 +40,17 @@ function GradientPic() {
     };
   }, []);
 
-    return (
-      <div className={styles.wrapper}>
-        {/* <ProgressBar progressCount={percentage} /> */}
-        <Timer />
-        <div className={styles.seoulBW} ></div>
-        <div style={{width: `${percentage}%`}} className={styles.seoulImg} ></div>
-      </div>
-    );
-  }
-  
-  export default GradientPic;
-  
+  return (
+    <div className={styles.wrapper}>
+      {/* <ProgressBar progressCount={percentage} /> */}
+      <Timer />
+      <div className={styles.seoulBW}></div>
+      <div
+        style={{ width: `${percentage}%` }}
+        className={styles.seoulImg}
+      ></div>
+    </div>
+  );
+}
+
+export default GradientPic;
